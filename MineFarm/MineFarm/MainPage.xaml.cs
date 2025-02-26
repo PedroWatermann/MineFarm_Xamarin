97fe5973d150e2a6fad87b5f0b8d4a21d35b8a10
PedroWatermann <pedro.programmer.pt@gmail.com>
2025-02-26 14:30:15 -0300

    App finalizado!

---
+++ MineFarm/MineFarm/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MineFarm
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPedregulho_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PagePedregulho(), true);
+        }
+
+        private void btnSair_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ MineFarm/MineFarm/PagePedregulho.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MineFarm
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PagePedregulho : ContentPage
+    {
+        public PagePedregulho()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVoltar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
